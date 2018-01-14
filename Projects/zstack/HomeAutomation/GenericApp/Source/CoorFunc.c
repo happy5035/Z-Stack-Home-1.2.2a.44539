e91a54451e09b2bbf5f035305d3e7f0d37f65dcb
yejiawei <xjtuyjw@163.com>
2018-01-14 15:48:23 +0800

    在report status后面添加rssi和lqi参数

--- Projects/zstack/HomeAutomation/GenericApp/Source/CoorFunc.c
+++ Projects/zstack/HomeAutomation/GenericApp/Source/CoorFunc.c
@@ -305,7 +305,18 @@ void CoorProcessEndStatus(afIncomingMSGPacket_t *pkt){
 //	osal_mem_free(buf);
 	
 #ifdef MT_TASK
-	MT_BuildAndSendZToolResponse(MT_RSP_CMD_APP, MT_APP_MSG, pkt->cmd.DataLength, pkt->cmd.Data);
+	uint8* packet;
+	uint16 len;
+	len = pkt->cmd.DataLength + 2;
+	packet = osal_mem_alloc(len);
+	if(packet){
+		osal_memcpy(packet,pkt->cmd.Data,pkt->cmd.DataLength);
+		*(packet + len - 2) = pkt->rssi;
+		*(packet + len - 1) = pkt->LinkQuality;
+		MT_BuildAndSendZToolResponse(MT_RSP_CMD_APP, MT_APP_MSG, len, packet);
+		osal_mem_free(packet);
+	}
+
 #endif
 }
 
